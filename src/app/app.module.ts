a3201430e18c9368bf0710a283bbc1bb6594ce7c
guy <guyasulin13@gmail.com>
2022-01-07 11:59:21 +0200

    add layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { TodoItemsListComponent } from './components/todo-items-list/todo-items-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    TodoItemsListComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/todo-items-list/todo-items-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-items-list',
+  templateUrl: './todo-items-list.component.html',
+  styleUrls: ['./todo-items-list.component.scss']
+})
+export class TodoItemsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
