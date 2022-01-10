21f4c84316c2b9d082f7ffca570c1f8f4fbae8c8
guy <guyasulin13@gmail.com>
2022-01-10 08:31:44 +0200

    fix scss and test

--- src/app/components/todo-items-list/todo-items-list.component.ts
+++ src/app/components/todo-items-list/todo-items-list.component.ts
@@ -23,7 +23,7 @@ export class TodoItemsListComponent implements OnInit {
   }
 
   pageEvent(event) {
-    this.pageIndex = event.pageIndex
+    this.pageIndex = event.pageIndex + 1
   }
 
   showItem(item) {
